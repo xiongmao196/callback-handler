e339d62382189ac834e4af5374c8d8a5eaa1cb5d
xiongmao196 <144508416+xiongmao196@users.noreply.github.com>
2024-07-29 13:19:44 +0800

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,8 +1,8 @@
 // pages/api/callback.js
-import crypto from 'crypto';
-
 export default function handler(req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'GET') {
+        res.status(200).send('success');
+    } else if (req.method === 'POST') {
         let body = '';
         req.on('data', chunk => {
             body += chunk.toString();
